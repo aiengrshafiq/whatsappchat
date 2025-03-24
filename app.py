aaf1ad5cf3cd2c591055c1997a2cd5c06d7eeb26
Muhammad Shafiq <engr.laravel@gmail.com>
2025-03-24 14:10:59 +0400

    send whatsapp messages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from utils import handle_whatsapp_webhook, validate_key
+from utils import handle_whatsapp_webhook, validate_key, handle_incoming_message,handle_whatsapp_precall_message_pipedrive
 import sys
 
 app = Flask(__name__)
@@ -8,10 +8,30 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route('/webhook', methods=['GET'])
+def whatsapp_webhook_get():
+    if request.method == 'GET':  # Meta Webhook Verification
+        challenge = request.args.get("hub.challenge")
+        verify_token = request.args.get("hub.verify_token")
+
+        if verify_token == VERIFY_TOKEN:
+            return challenge  # Meta expects the raw challenge value as response
+        else:
+            return "Verification failed", 403
+
+
 @app.route('/webhook', methods=['POST'])
 def whatsapp_webhook():
-    data = request.get_json()
-    
+    data = request.json
+    if 'entry' in data:
+        for entry in data['entry']:
+            for change in entry.get('changes', []):
+                if change.get('field') == 'messages':
+                    message_data = change['value']
+                    handle_incoming_message(message_data)
+    return jsonify(success=True), 200
+
     # Call the refactored function in utils.py to handle the webhook logic
     #reply, user_data = handle_whatsapp_webhook(data)
     
@@ -20,7 +40,14 @@ def whatsapp_webhook():
     # send_whatsapp_message(user_data["from"], reply)
     
     #return jsonify({"reply": reply, "user_data": user_data})
-    return jsonify({"reply": "Test Reply", "user_data": data})
+    #return jsonify({"reply": "Test Reply", "user_data": data})
+
+@app.route('/send_whatsapp_precall_message_pipedrive', methods=['POST'])
+def send_whatsapp_message():
+    data = request.get_json()
+    rsponse = handle_whatsapp_precall_message_pipedrive(data)
+    return rsponse
+
 
 @app.route('/validate_key')
 def validate_key_endpoint():
